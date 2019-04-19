a25092bf4f19948e8a7781ddbcc50072cdf7b04d
DhruvBansal86 <goldbrickdhruv@gmail.com>
2019-04-19 09:16:38 +0530

    Update main.tf

--- terraform_new/modules/sg/main.tf
+++ terraform_new/modules/sg/main.tf
@@ -37,6 +37,12 @@ resource "aws_security_group" "public_machine" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
     egress {
         from_port = 8005
